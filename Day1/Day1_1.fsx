41edd51d1e39a4af1d8bffb7fa79543bf45c4bf2
Gerke Kooistra <gerke_kooistra@live.nl>
2025-12-01 23:11:36 +0100

    day1

---
+++ Day1/Day1_1.fsx
@@ -0,0 +1,37 @@
+﻿#r "nuget: FParsec"
+open FParsec
+
+open System.IO
+open Microsoft.FSharp.Collections
+
+let input = File.ReadAllText "input.txt" 
+
+type Direction = 
+    | Right
+    | Left
+
+type Rotation = { 
+    direction : Direction
+    distance : int
+}
+
+let parse p str =
+    match run p str with
+    | Success(result, _, _) -> result
+    | Failure(msg,_,_) -> failwith $"Parse error: {msg}"
+
+let  directionParser = (stringReturn "R"  Right) <|> (stringReturn "L" Left) .>>. pint32 .>> spaces
+
+let parsedInput = parse (many directionParser) input |> List.map (fun (dir, step) -> {direction = dir; distance = step }) 
+
+let rotate current {direction = direction; distance = distance}  =
+    let rawCurrent = 
+        match direction with
+        | Right -> (current + distance)%100
+        | Left -> (current - distance)%100
+    if rawCurrent < 0 then rawCurrent + 100 else rawCurrent
+
+
+let outcome = List.scan rotate 50 parsedInput |> List.filter (fun a -> a = 0) |> List.length  
+
+printf "%A" outcome
---
+++ Day1/Day1_2.fsx
@@ -0,0 +1,49 @@
+﻿#r "nuget: FParsec"
+open FParsec
+
+open System.IO
+open Microsoft.FSharp.Collections
+
+let input = File.ReadAllText "input.txt" 
+
+type Direction = 
+    | Right
+    | Left
+
+type Rotation = { 
+    direction : Direction
+    distance : int
+}
+
+let parse p str =
+    match run p str with
+    | Success(result, _, _) -> result
+    | Failure(msg,_,_) -> failwith $"Parse error: {msg}"
+
+let directionParser = (stringReturn "R"  Right) <|> (stringReturn "L" Left) .>>. pint32 .>> spaces
+
+let parsedInput = parse (many directionParser) input |> List.map (fun (dir, step) -> {direction = dir; distance = step }) 
+
+let rotate position {direction = direction; distance = distance}  =
+    let rawPosition = 
+        match direction with
+        | Right -> (position + distance)%100
+        | Left -> (position - distance)%100
+    if rawPosition < 0 then rawPosition + 100 else rawPosition
+
+let getClicks position {direction = direction; distance = distance} =
+    let initClicks = distance/100
+    let additionClicks = 
+        match direction with
+        | Right -> if position + distance%100 >= 100 then 1 else 0
+        | Left -> if position <> 0 && position - distance%100 <= 0 then 1 else 0
+    initClicks + additionClicks
+
+let rotateWithClicks (clicks, current) rotation =
+    let addedClicks = getClicks current rotation
+    let newPosition = rotate current rotation
+    (clicks + addedClicks, newPosition)
+
+let clicks = List.scan rotateWithClicks (0,50) parsedInput |> List.last |> fun (clicks, position) -> clicks
+
+printf "%A" clicks
